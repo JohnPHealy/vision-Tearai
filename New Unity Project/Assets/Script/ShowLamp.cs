06f947dccee5273d3dedb521a7c348caaa6880ad
Tearai <77899947+Tearai@users.noreply.github.com>
2021-05-06 23:28:49 +0100

    Implementations after playtesting
    added some features onto the game after playtesting feedback

---
+++ New Unity Project/Assets/Script/ShowLamp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowLamp : MonoBehaviour
+{
+    public GameObject txtToDisplay;
+
+    private bool Inside;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Inside = true;
+
+        if (other.gameObject.tag == "Player" && Inside == true)
+        {
+            txtToDisplay.SetActive(true);
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Inside = false;
+        if (other.gameObject.tag == "Player" && Inside == false)
+        {
+            txtToDisplay.SetActive(false);
+            this.gameObject.SetActive(false);
+        }
+    }
+
+}
